18c37b572a30003e8743028bfdbd199729ded4f9
lab04 <ieepo@felipe.local>
2015-02-04 18:30:19 -0600

    Add button

---
+++ DBManager.h
@@ -0,0 +1,13 @@
+//
+//  DBManager.h
+//  Lab03
+//
+//  Created by felipe on 04/02/15.
+//  Copyright (c) 2015 felipe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DBManager : NSObject
+
+@end
---
+++ DBManager.m
@@ -0,0 +1,13 @@
+//
+//  DBManager.m
+//  Lab03
+//
+//  Created by felipe on 04/02/15.
+//  Copyright (c) 2015 felipe. All rights reserved.
+//
+
+#import "DBManager.h"
+
+@implementation DBManager
+
+@end
